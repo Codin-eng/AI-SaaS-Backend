e057857fe06038151587eac9d45360b562dbb47c
codin-eng <bratha500@gmail.com>
2026-05-28 12:00:00 +0300

    remove frontend from backend repo

--- frontend/src/app/components/Loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="p-4 text-gray-500">
-      Loading...
-    </div>
-  );
-}
--- frontend/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function HomePage() {
-  return (
-    <div className="p-10">
-      <h1 className="text-4xl font-bold">
-        SaaS App
-      </h1>
-    </div>
-  );
-}
